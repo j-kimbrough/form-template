f448887b877b09a687bb935fd28bf97704e0fe1a
Jade Kimbrough <jadekimbrough123@gmail.com>
2022-01-11 08:57:06 -0800

    creates Form with JS, css, and HTML

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 //alert("test");
 
 //https://tinyurl.com/dynamic-html-checker
+
+let myDate = new Date();
+let myYear =  myDate.getFullYear();
+document.getElementById("copyright").innerHTML = myYear;
+
 document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
 
 document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);  
+
 
 //manages mobile nav 
 function myFunction() {
